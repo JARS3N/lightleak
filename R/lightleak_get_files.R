1c555d8a866ffbc5c93ee80761b56aed462aa2cd
JARS3N <JARS3N@users.noreply.github.com>
2018-09-06 10:04:31 -0400

    Update lightleak_get_files.R

--- R/lightleak_get_files.R
+++ R/lightleak_get_files.R
@@ -3,8 +3,7 @@ get_files<-  function(dir,opensavexlsx=F){
   if (!opensavexlsx == T) {
     xprt::open_save_xl_wb(dir)
   }
-  list.files(dir,recursive = T,pattern = 'xlsx',full.names = T) %>%
-    lapply(.,mungeLL)
+  lapply(list.files(dir,recursive = T,pattern = 'xlsx',full.names = T),mungeLL)
   # needs to add a two part upload to SQL db
   # 1. add meta data to table and get index for key
   # 2. add key to data df and upload that to othertable
