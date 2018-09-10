360fbb91910f9d248de599b06e3e89e3db31c39e
JARS3N <JARS3N@users.noreply.github.com>
2018-09-10 10:24:08 -0400

    Create lightleak_pull_meta.R

---
+++ R/lightleak_pull_meta.R
@@ -0,0 +1,20 @@
+pull_meta <- function(XL, xlfile) {
+  nms <- c(
+    "Light Leak Test",
+    "Serial number:",
+    "Date:",
+    "Time Start:",
+    "Time End:",
+    "Final result:",
+    "High Limit Multiplicator:",
+    "Low Limit Multiplicator:",
+    "Num Points:"
+  )
+  met <-
+    setNames(as.data.frame(lapply(nms, get_term_value, data = XL)), gsub("[ ]|[:]", "", nms))
+  met <- met[, order(names(met))]
+  met$file <- xlfile
+  met
+}
+
+
