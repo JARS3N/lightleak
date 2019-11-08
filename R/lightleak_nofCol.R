514da1430cd54e627c11c71873b3564c9de9a489
JARS3N <JARS3N@users.noreply.github.com>
2019-11-08 10:27:32 -0500

    Update lightleak_nofCol.R

--- R/lightleak_nofCol.R
+++ R/lightleak_nofCol.R
@@ -2,12 +2,12 @@
 # because data format from Utility is inconsistent
 nofCol <- function(u) {
   x <- substr(rm_leading_zero(u),1,2)
-  list(
+  unlist(list(
     "42" = 6,
     "41" = 12,
     "43" = 1,
     "21" = 12,
     "10" = 6,
     "44" = 1
-  )[x]
+  )[x])
 }
