bee2d72afb850f8d055797d2ef2ddc6c0a351a36
JARS3N <JARS3N@users.noreply.github.com>
2022-08-10 14:49:10 -0400

    Update app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -2,7 +2,7 @@ app_ui<-function(){
 library(shiny)
 shinyUI(fluidPage(fluidRow(
   mainPanel(width = 15,height = 100,
-            column(width = 1,selectInput("plat", "Platform", c("e24" = 42,"e96" = 41,"XFp" = 43),
+            column(width = 1,selectInput("plat", "Platform", c("e24" = 42,"e96" = 41,"XFp" = 43,"XFPro" = 45),
                                          selected = 42, multiple = FALSE,width = 100)),
             column(width = 1,selectInput("analyte", "Analyte", c("O2","pH"),
                                          selected = NULL, multiple = FALSE,width = 100)),
