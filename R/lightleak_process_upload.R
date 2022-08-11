81fbe53ccc7a807ce8275dee335459953ed77c9d
JARS3N <JARS3N@users.noreply.github.com>
2022-08-11 14:13:09 -0400

    Update lightleak_process_upload.R

--- R/lightleak_process_upload.R
+++ R/lightleak_process_upload.R
@@ -1,28 +1,22 @@
-process_upload <- function(U, db) {
-  # upload meta data which gives it an ID#
-  dbWriteTable(
-    my_db,
-    name = "instqcllmeta",
-    value = U$meta,
-    append = T,
-    overwrite = F,
-    row.names = FALSE
-  )
-  # get ID number
-  query <- RMySQL::dbSendQuery(db,
-                               paste0('SELECT ID from instqcllmeta where file="', U$meta$file, ';"'))
+process_upload <- function(U,db)
+{
+  #cleanse file path
+  U$meta$file <- basename(U$meta$file)
+  # remove leading zeros from instrument serial
+  U$meta$Serialnumber <- gsub("^00","",U$meta$Serialnumber)
+  #copy meta data over
+  dbWriteTable(db, name = "instqcllmeta", value = U$meta, 
+               append = T, overwrite = F, row.names = FALSE)
+  # query to get auto enumerated ID
+ qstring <- gsub("%%%",shQuote(U$meta$file),
+                 "SELECT ID from instqcllmeta where file=%%%;")
+  query <- RMySQL::dbSendQuery(con=my_db,
+                               statement = qstring)
   runID <- RMySQL::dbFetch(query)
   RMySQL::dbClearResult(query)
-  #add Ide number to data.frame
+  # add meta ID to data
   U$data$MetaID <- runID$ID
-  #uploadinstQClldata
-  dbWriteTable(
-    my_db,
-    name = "instqclldata",
-    value = U$data,
-    append = T,
-    overwrite = F,
-    row.names = FALSE
-  )
-
+  # add LL data to LL data table
+  dbWriteTable(my_db, name = "instqclldata", value = U$data, 
+               append = T, overwrite = F, row.names = FALSE)
 }
